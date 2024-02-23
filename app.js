00451a5749cd716683582f276ae15d202ca8d358
B2000144 <phamhuutri101@gmail.com>
2024-02-23 15:50:18 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const contactsRouter = require("./app/route/contact.route");
 const cors = require("cors");
  const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => { 
     res.json({message: "welcome to contact book application."});
